b589e337b1e4acb240e6d943960e1d9b573c274a
早川拓人 <takuto.hayakawa@soliton.co.jp>
2016-10-03 18:24:27 +0900

    Initial Commit

---
+++ shuffleMusic/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  shuffleMusic
+//
+//  Created by 早川拓人 on 2016/10/03.
+//  Copyright © 2016年 早川拓人. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
